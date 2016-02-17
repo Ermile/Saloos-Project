89132baaac69fbdd76a1c2d7d2e9399ab8fd83b3
Javad <J.Evazzadeh@live.com>
2016-02-17 15:46:05 +0330

    update trans

--- includes/languages/trans_twig.php
+++ includes/languages/trans_twig.php
@@ -6,5 +6,9 @@ function transtext()
 	echo T_("Comming Soon");                                                          // Line 11
 	echo T_("Powered by Saloos");                                                     // Line 11
 
+	//------------------------------------project\includes\languages\trans_static.html
+	echo T_("Saloos");                                                                // Line 7 Seperate
+	echo T_(" ");                                                                     // Line 8 Seperate
+
 }
 ?>
