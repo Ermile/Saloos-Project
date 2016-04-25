994bda446fc07842cddb214ff3b66936514ab4f5
Javad <J.Evazzadeh@live.com>
2016-04-25 12:40:16 +0430

    show result if debug of telegram is on

--- content/saloos_tg/controller.php
+++ content/saloos_tg/controller.php
@@ -12,7 +12,7 @@ class controller extends \lib\mvc\controller
 		if($this->url('path') == $myhook)
 		{
 			$result = self::tg_handle();
-			if(DEBUG)
+			if(\lib\utility\option::get('telegram', 'meta', 'debug'))
 			{
 				var_dump($result);
 			}
