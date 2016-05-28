8c7f3d70480da31064206335dcbe3ea02054086b
Javad <J.Evazzadeh@live.com>
2016-05-28 19:23:27 +0430

    create template for pages and posts and fix translation

--- content/home/view.php
+++ content/home/view.php
@@ -5,10 +5,14 @@ class view extends \mvc\view
 {
 	function config()
 	{
-		$this->include->css          = false;
 		// $this->include->css_ermile   = false;
 		$this->include->js           = false;
-		$this->include->js_main      = false;
+		if($this->module() === 'home')
+		{
+			$this->data->bodyclass  = 'unselectable';
+			$this->include->js_main      = false;
+			$this->include->css          = false;
+		}
 	}
 }
 ?>
